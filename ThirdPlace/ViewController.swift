0f6133356c2c0055c49cc34ca780f1d0bd5ad054
taaapo <takupow@icloud.com>
2024-02-06 01:33:32 +0900

    Initial Commit

---
+++ ThirdPlace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdPlace
+//
+//  Created by 恵紙拓玖 on 2024/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
