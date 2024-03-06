ef6698d013547e85ac212063c85a6100e61610d6
taaapo <takupow@icloud.com>
2024-03-07 06:32:06 +0900

    avatar imageの登録まで完了した※load user でのimageのダウンロードはまだ

---
+++ ThirdPlace/Helper/FileStorage.swift
@@ -0,0 +1,20 @@
+//
+//  FileStorage.swift
+//  ThirdPlace
+//
+//  Created by 恵紙拓玖 on 2024/03/05.
+//
+
+import Foundation
+import FirebaseStorage
+import UIKit
+
+let storage = Storage.storage()
+
+class FileStorage {
+    
+    class func uploadImage(_ image: UIImage, directory: String, completion: @escaping (_ documentLink: String?) -> Void) {
+        
+        
+    }
+}
