b93ad94d0645da6afe648d43dd040975341692c4
taaapo <takupow@icloud.com>
2024-04-01 01:35:26 +0900

    170まで完了

---
+++ ThirdPlace/ViewController/LikesViewController/LikeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LikeTableViewCell.swift
+//  ThirdPlace
+//
+//  Created by 恵紙拓玖 on 2024/03/31.
+//
+
+import UIKit
+
+class LikeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
