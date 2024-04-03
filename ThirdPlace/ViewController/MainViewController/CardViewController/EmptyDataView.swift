c735b9558265e2762aee9de6a3f2cfe21273e676
taaapo <takupow@icloud.com>
2024-04-04 07:57:07 +0900

    EmptyDataViewまで完了

---
+++ ThirdPlace/ViewController/MainViewController/CardViewController/EmptyDataView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyDataView.swift
+//  ThirdPlace
+//
+//  Created by 恵紙拓玖 on 2024/04/03.
+//
+
+import UIKit
+
+class EmptyDataView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
